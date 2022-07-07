5676e0ddaf51a2ea4f71cae37d10037f87facdad
jiuer12138 <18815283494@163.com>
2022-07-07 18:00:09 +0800

    添加了自定权限组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css"
+import "./assets/fonts/iconfont.css"
+import axios from 'axios'
+Vue.directive('focus',{
+  inserted(ele){
+    ele.focus()
+  }
+})
 
+Vue.directive('permission',{
+  inserted(ele,binding,vnode){
+    const arr=['admin','test','developer']//判断条件
+      if(arr.includes(vnode.context[binding.expression])) {
+        ele.style.display='inline-block';
+      }else{
+        ele.style.display='none'
+      }
+  }
+})
+axios.defaults.baseURL='https://www.escook.cn'
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/MyGoodsSearch.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+MyGoodsSearch
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components:{
+
+  },
+  mounted(){
+
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+}
+</script>
+<style scoped>
+
+</style>
